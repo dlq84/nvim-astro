e096f9fb95a66460cdfff6d4aaabc6f90a4dde38
Daniel L <daniel.lundqvist84@gmail.com>
2023-10-19 16:01:22 +0200

    add a bunch of packs from astro community, disable format on tsserver and html lsp

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,20 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- themes
+  { import = "astrocommunity.colorscheme.vscode-nvim"},
+
+  -- languages
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.bash" },
 }
