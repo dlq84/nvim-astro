afe60584d861187f21de78dbed556a6c737c466b
Daniel L <daniel.lundqvist84@gmail.com>
2024-05-10 09:41:14 +0200

    fix settings and remove lock file from git

--- lua/community.lua
+++ lua/community.lua
@@ -11,17 +11,13 @@ return {
 
   { import = "astrocommunity.bars-and-lines.scope-nvim" },
   { import = "astrocommunity.bars-and-lines.vim-illuminate" },
-  --  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
-  -- { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.syntax.hlargs-nvim" },
-  { import = "astrocommunity.lsp.lsp-signature-nvim" },
-  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
-  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.utility.noice-nvim" },
 
   -- languages
   { import = "astrocommunity.pack.angular" },
@@ -38,4 +34,8 @@ return {
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.python" },
+
+  -- recipes
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
 }
